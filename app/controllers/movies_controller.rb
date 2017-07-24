98706d1ce07f7957d1947bf7c1d29486a50cfd6d
Pooja Parthasarathy <ppartha0@chicagobooth.edu>
2017-07-24 21:19:23 +0000

    Added more director code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
     
     def index_directors
+        @p = Director.all
         render("/movie_templates/index_directors_template.html.erb")
+    end
+    
+    def show_director
+        
+        render("/movie_templates/show_director_template.html.erb")
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get("/directors", {:controller => "movies", :action => "index_directors"})
+  get("/directors/:the_id", {:controller => "movies", :action => "show_director"})
+  
   mount WebGit::Engine, at: "/rails/git"
 end
