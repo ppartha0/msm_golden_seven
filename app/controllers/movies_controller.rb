e7b41dd28cfbfe23463a3f1e1e3d8821a00111b9
Pooja Parthasarathy <ppartha0@chicagobooth.edu>
2017-07-24 21:29:09 +0000

    Made all the Read routes work

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 class MoviesController < ApplicationController
     
+    ##### READ
     def index_directors
         @p = Director.all
         render("/movie_templates/index_directors_template.html.erb")
@@ -8,6 +9,26 @@ class MoviesController < ApplicationController
     def show_director
         
         render("/movie_templates/show_director_template.html.erb")
+    end
+    
+    def index_actors
+        @p = Actor.all
+        render("/movie_templates/index_actors_template.html.erb")
+    end
+    
+    def show_actor
+        
+        render("/movie_templates/show_actor_template.html.erb")
+    end
+    
+    def index_movies
+        @p = Movie.all
+        render("/movie_templates/index_movies_template.html.erb")
+    end
+    
+    def show_movie
+        
+        render("/movie_templates/show_movie_template.html.erb")
     end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,18 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
+  get("/", {:controller => "movies", :action => "index_movies"})
+  
+  # READ
   get("/directors", {:controller => "movies", :action => "index_directors"})
   get("/directors/:the_id", {:controller => "movies", :action => "show_director"})
+  
+  get("/actors", {:controller => "movies", :action => "index_actors"})
+  get("/actors/:the_id", {:controller => "movies", :action => "show_actor"})
+  
+  get("/movies", {:controller => "movies", :action => "index_movies"})
+  get("/movies/:the_id", {:controller => "movies", :action => "show_movie"})
+  
   
   mount WebGit::Engine, at: "/rails/git"
 end
